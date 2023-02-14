f6b885b0cf8e6422c7d843cb500df7ea0a34370d
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2023-02-13 21:50:39 -0700

    linking between pages and blackjack setup

---
+++ src/BlackJackGame.js
@@ -0,0 +1,6 @@
+function BlackJackGame() {
+    return (
+        <h1>*gotta put in code*</h1>
+    )
+}
+export default BlackJackGame
--- src/Header.js
+++ src/Header.js
@@ -6,15 +6,26 @@ import diamond from './diamond.png';
 import club from './club.png';
 import heart from './heart.png';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function MakeHeader() { 
     return(
         <div>
             <header>
                 {/* make these buttons have options */}
-                <button class='headerBtn'>Single Player</button>
-                <img src={logo} alt='logo' className='headerLogo'></img>
-                <button class='headerBtn'>Multiplayer</button>
+                <Link to="/Home.js"><img src={logo} alt='logo' className='headerLogo'></img></Link>
+                <div className='dropDown'>
+                    <button className='singlePlayerBtn headerBtn'>Single Player</button>
+                    <div className="gamesDropdown">
+                        <Link to="/BlackJack.js" className="navLink">Black Jack</Link>
+                    </div>
+                </div>
+                <div className='dropDown'>
+                    <button className='multiPlaerBtn headerBtn'>Multiplayer</button>
+                    <div className='gamesDropdown'>
+                        
+                    </div>
+                </div>
             </header>
             <div id='suitsDiv'>
                 <img src={spade} alt='spade' className='headerSuit'></img>
